cc28ab9505cd522d568c4d4c1c06cb56fc14cc9d
Randall Blake <rkb.kirby@gmail.com>
2023-07-08 16:51:52 -0400

    added cameracontroller

--- Floor.gd
+++ Floor.gd
@@ -1,15 +1,25 @@
 extends RigidBody
 
 
-const SPEED = 100
+const SPEED = 5000
 
+func get_input() -> Vector3:
+	var input := Vector3.ZERO
+	if Input.is_action_pressed('ui_right') and angular_velocity.z > -1:
+		input.z = -1
+	if Input.is_action_pressed('ui_left') and angular_velocity.z < 1:
+		input.z = 1
+	if Input.is_action_pressed('ui_up') and angular_velocity.x > -1:
+		input.x = -1
+	if Input.is_action_pressed('ui_down') and angular_velocity.x < 1:
+		input.x = 1
+		
+	return input
 
 func _physics_process(delta):
-	if Input.is_action_pressed('ui_right'):
-		add_torque(Vector3(0,0,-SPEED))
-	if Input.is_action_pressed('ui_left'):
-		add_torque(Vector3(0,0,SPEED))
-	if Input.is_action_pressed('ui_up'):
-		add_torque(Vector3(-SPEED,0,0))
-	if Input.is_action_pressed('ui_down'):
-		add_torque(Vector3(SPEED,0,0))
+	var input = get_input().normalized()
+	if input != Vector3.ZERO:
+		add_torque(input * SPEED)
+	else:
+#		print(angular_velocity)
+		angular_velocity = angular_velocity.move_toward(Vector3.ZERO, 0.5 * delta)
